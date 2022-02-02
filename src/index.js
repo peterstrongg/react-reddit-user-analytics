599170e48c31f008a4b54d570dfc4c3b15bd9515
Peter <peterstrong8343@gmail.com>
2022-02-02 15:29:07 -0500

    gh pages

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import store from './store/index';
 
 
 ReactDOM.render(
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Provider store={store}>
             <App />
         </Provider>
